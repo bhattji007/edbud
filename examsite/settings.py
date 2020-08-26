ada995da343ef1d1a80eb897b3c8f57fa14a8481
MishraDhruv26 <mishradhruv117@gmail.com>
2020-08-26 14:28:52 +0530

    Update settings.py

--- examsite/settings.py
+++ examsite/settings.py
@@ -126,8 +126,8 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'edbudexaminations@gmail.com'
-EMAIL_HOST_PASSWORD = 'web@edbud26'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 # Managing media, style sheet etc
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
